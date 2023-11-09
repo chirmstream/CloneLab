b985a62e5cd28e76773fa3eb1ead4dd321d25f7e
chirmstream <dextergbarney@gmail.com>
2023-11-09 10:57:08 -0800

    git.Repo constructor takes two url as arguments now
    Signed-off-by: chirmstream <dextergbarney@gmail.com>

--- clone_and_push_mirror.py
+++ clone_and_push_mirror.py
@@ -4,7 +4,7 @@ import git
 original_url = "https://github.com/dhouck/anti-creeper-grief.git"
 mirror_url = "https://github.com/chirmstream/CloneLab-Testing.git"
 
-repo = git.Repo(original_url)
+repo = git.Repo(original_url, mirror_url)
 repo.clone()
 
 github_personal_access_token = "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"
