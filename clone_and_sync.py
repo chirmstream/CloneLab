e99b6b82d5d3bff8d2455ffcdaffb9e3cfbef9a5
chirmstream <dextergbarney@gmail.com>
2023-11-09 14:16:21 -0800

    working rsync, github authentication still needs work
    Signed-off-by: chirmstream <dextergbarney@gmail.com>

--- clone_and_sync.py
+++ clone_and_sync.py
@@ -4,7 +4,7 @@ import git
 original_url = "https://github.com/dhouck/anti-creeper-grief.git"
 mirror_url = "https://github.com/chirmstream/CloneLab-Testing.git"
 
-github_personal_access_token = "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"
+github_personal_access_token = "token"
 
 repo = git.Repo(original_url, mirror_url)
 repo.clone()
