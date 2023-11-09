dd5d10f81fa0b0d9c347e0be078eba891c09d881
chirmstream <dextergbarney@gmail.com>
2023-11-09 14:27:12 -0800

    commiting a public repo

--- clone_and_sync.py
+++ clone_and_sync.py
@@ -4,7 +4,7 @@ import git
 original_url = "https://github.com/dhouck/anti-creeper-grief.git"
 mirror_url = "https://github.com/chirmstream/CloneLab-Testing.git"
 
-github_personal_access_token = "token"
+github_personal_access_token = "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"
 
 repo = git.Repo(original_url, mirror_url)
 repo.clone()
