e5feaf2dfba9751ccd45cd9031c9fa8059d05215
chirmstream <dextergbarney@gmail.com>
2023-11-10 21:29:23 -0800

    Included a gitlab test

--- clonelab.py
+++ clonelab.py
@@ -2,12 +2,16 @@ import git
 
 
 original_url = "https://github.com/chirmstream/CloneLab.git"
+
 mirror_url = "https://github.com/chirmstream/CloneLab-Testing.git"
-github_personal_access_token = "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"
+#mirror_url = "https://git.nasdex.net/chirmstream/clonelab-testing.git"
+
+github_personal_access_token = "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"
+gitlab_token = "glpat-ZsF8Ff4mLyWyaMs_XsjW"
 
 repo = git.Repo(original_url, mirror_url)
 repo.get()
-repo.mirror_auth(github_personal_access_token)
+repo.set_mirror_login(github_personal_access_token)
 repo.sync()
 repo.add()
 repo.commit("this is a test commit")
