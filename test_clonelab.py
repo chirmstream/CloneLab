50740585584412cafd9dad489d909323b489561b
chirmstream <dextergbarney@gmail.com>
2023-11-14 14:48:01 -0800

    repo mirroring works for all 3 example repositories using test_clonelab.py
    Signed-off-by: chirmstream <dextergbarney@gmail.com>

--- test_clonelab.py
+++ test_clonelab.py
@@ -1,19 +1,19 @@
 import git
+import os
 import csv
 
 
 with open("config.example", "r") as csvfile:
-    #fieldnames = csv.DictReader(csvfile)
-    #fieldnames = fieldnames.fieldnames
-    #reader = csv.DictReader(csvfile, fieldnames=fieldnames)
-
     reader = csv.DictReader(csvfile)
     fieldnames = reader.fieldnames
+    os.chdir("..")
     for row in reader:
         repo = git.Repo(row["original_repository"], row["mirror_repository"])
         repo.get()
         repo.set_mirror_login(row["mirror_password"])
         repo.sync()
         repo.add()
-        repo.commit("this is a test commit")
+        repo.commit("CloneLab autocommit")
         repo.push()
+
+print("script finished?")
