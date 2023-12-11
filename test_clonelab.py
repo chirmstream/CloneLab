7b94f046948bf2e5bf46174056fd74b83a12a734
chirmstream <dextergbarney@gmail.com>
2023-12-11 15:55:54 -0800

    added sync function

--- test_clonelab.py
+++ test_clonelab.py
@@ -3,15 +3,24 @@ import os
 import csv
 
 
-# Sync mirror repors
-with open("config.csv", "r") as csvfile:
-    reader = csv.DictReader(csvfile)
-    fieldnames = reader.fieldnames
-    os.chdir("..")
-    for row in reader:
-        repo = git.Repo(row["original_repository"], original)
-        mirror_repo = git.Repo(row["mirror_repository"], mirror)
-        repo.sync()
+def main():
+    # Sync mirror repors
+    with open("config.csv", "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        fieldnames = reader.fieldnames
+        os.chdir("..")
+        for row in reader:
+            repo = git.Repo(row["original_repository"], original)
+            mirror_repo = git.Repo(row["mirror_repository"], mirror)
+            repo.sync()
 
-print("CLoneLab finished!  All repositories have been mirrored.")
-print("Exiting")
+    print("CLoneLab finished!  All repositories have been mirrored.")
+    print("Exiting")
+
+
+def sync(repo, mirror_repo):
+    repo.get()
+    mirror_repo.get()
+
+
+main()
